e1da842cd98f226e7b0b83213263afb7a0f87e7a
warmans <stefan.warman@gmail.com>
2016-11-10 20:11:35 +0100

    Minor changes to search component behaviour

--- ui/src/app/component/shared/event-search/event-search.component.ts
+++ ui/src/app/component/shared/event-search/event-search.component.ts
@@ -15,28 +15,34 @@ export class EventSearchComponent implements OnInit {
 
   ngOnInit() {
     this.filters = [
-      Filter.NewFilter('happening...', 'date_range', 'happening', ['today', 'tomorrow', 'this weekend', 'this week'], true),
-      Filter.NewFilter('of type...', 'types', 'of type', ['concert'], true)
+      Filter.NewFilter('happening...', 'date_range', 'happening', ['today', 'tomorrow', 'this weekend', 'this week'], true, 'this weekend'),
+      Filter.NewFilter('of type...', 'types', 'of type', ['concert'], true, 'concert')
     ];
   }
 
   addFilter(index: number) {
     let added = this.filters.splice(index, 1);
     this.filterSelection.push(added[0]);
+    this.upateQuery();
   }
 
   removeFilter(index: number) {
     let removed = this.filterSelection.splice(index, 1);
     this.filters.push(removed[0]);
+    this.upateQuery();
   }
 
   isSelected(filter: Filter): boolean {
     for (let v of this.filterSelection) {
       if (filter.field === v.field) {
-          return true;
+        return true;
       }
     }
     return false;
+  }
+
+  upateQuery() {
+
   }
 }
 
@@ -48,10 +54,18 @@ class Filter {
   valueChoices: string[];
   isSingleChoice: boolean;
 
-  static NewFilter(name: string, field: string, description: string, valueChoices: string[], isSingleChoice: boolean): Filter {
+  static NewFilter(
+    name: string,
+    field: string,
+    description: string,
+    valueChoices: string[],
+    isSingleChoice: boolean,
+    defaultValue: string
+  ): Filter {
     let f = new Filter();
     f.name = name;
     f.field = field;
+    f.value = defaultValue;
     f.description = description;
     f.valueChoices = valueChoices;
     f.isSingleChoice = isSingleChoice;
