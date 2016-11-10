4b0b23423e2372046d9ed7aa34c96af90eb201db
warmans <stefan.warman@gmail.com>
2016-11-10 19:45:32 +0100

    Fixed test errors. Use PhantomJS instead of Chrome in test runner

--- ui/src/app/component/shared/event-list/event-list.component.spec.ts
+++ ui/src/app/component/shared/event-list/event-list.component.spec.ts
@@ -6,18 +6,31 @@ import { MockBackend } from '@angular/http/testing';
 import { BaseRequestOptions, Http, HttpModule } from '@angular/http';
 import { EventService } from './../../../service/event/event.service';
 import { UserService } from './../../../service/user/user.service';
-
-
+import { FormsModule } from '@angular/forms';
+import { UtagsComponent } from './../utags/utags.component';
+import { MomentModule } from 'angular2-moment';
+import { ShortenText } from './../../../pipe/shorten-text.pipe';
 
 describe('Component: EventList', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EventListComponent],
-      providers: [],
-      imports: [HttpModule]
+      declarations: [EventListComponent, UtagsComponent, ShortenText],
+      providers: [
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        EventService,
+        UserService
+      ],
+      imports: [HttpModule, FormsModule, MomentModule]
     });
   });
+
 
   it('should create an instance', () => {
     const fixture = TestBed.createComponent(EventListComponent);
--- ui/src/app/pipe/shorten-text.pipe.ts
+++ ui/src/app/pipe/shorten-text.pipe.ts
@@ -1,15 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
 
 
-@Pipe({name: 'shorten'})
+@Pipe({ name: 'shorten' })
 export class ShortenText implements PipeTransform {
-  constructor(){}
-
   transform(text: string, maxLen: number): any {
-      if (text.length <= maxLen) {
-        return text;
-      }
-      return text.slice(0, maxLen) + '...';
+    if (text.length <= maxLen) {
+      return text;
+    }
+    return text.slice(0, maxLen) + '...';
   }
 }
